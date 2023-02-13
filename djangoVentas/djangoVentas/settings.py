d72a637c4379069137b8bae6187f8dcdde21ba27
Frank Puertas <franktoomg@gmail.com>
2023-02-13 10:19:28 -0500

    a

--- djangoVentas/djangoVentas/settings.py
+++ djangoVentas/djangoVentas/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f)1i%0m630+sw^5@q1r)8p6+cgzwcw@s33jjjkj(sba99e4-%j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
